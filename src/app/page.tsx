feaa415703f6838e46a1738807f3eac1f0491ffc
Mudassar Rehman <mudassar.rehman2575@gmail.com>
2023-08-20 17:23:18 +0500

    removed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Image from 'next/image';
 import React, { useState } from 'react';
-import ContactList from '../../components/ContactList';
 import SearchBar from '../../components/SearchBar';
 
 export default function Home() {
